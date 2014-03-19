3c10cb45b357427661bc48697a5f3887cb8a5016
Mike Ryan <falter@gmail.com>
2014-03-19 14:59:55 -0500

    Improve logging specs

--- spec/logging_spec.rb
+++ spec/logging_spec.rb
@@ -12,28 +12,51 @@ describe Cikl::Worker::Logging do
   after :all do
     Cikl::Worker.logger = @original_logger
   end
-  let(:logger) {double('logger') }
 
-  before :each do
-    @obj = TestLogging.new
-    Cikl::Worker.logger = logger
+  describe "an example logger" do
+    let(:logger) {double('logger') }
+
+    before :each do
+      @obj = TestLogging.new
+      Cikl::Worker.logger = logger
+    end
+
+    specify "#error should call Cikl::Worker.logger.error" do
+      expect(logger).to receive(:error).with("this is an error")
+      @obj.error("this is an error")
+    end
+    specify "#warn should call Cikl::Worker.logger.warn" do
+      expect(logger).to receive(:warn).with("this is an warn")
+      @obj.warn("this is an warn")
+    end
+    specify "#info should call Cikl::Worker.logger.info" do
+      expect(logger).to receive(:info).with("this is an info")
+      @obj.info("this is an info")
+    end
+    specify "#debug should call Cikl::Worker.logger.debug" do
+      expect(logger).to receive(:debug).with("this is an debug")
+      @obj.debug("this is an debug")
+    end
   end
 
-  specify "#error should call Cikl::Worker.logger.error" do
-    expect(logger).to receive(:error).with("this is an error")
-    @obj.error("this is an error")
-  end
-  specify "#warn should call Cikl::Worker.logger.warn" do
-    expect(logger).to receive(:warn).with("this is an warn")
-    @obj.warn("this is an warn")
-  end
-  specify "#info should call Cikl::Worker.logger.info" do
-    expect(logger).to receive(:info).with("this is an info")
-    @obj.info("this is an info")
-  end
-  specify "#debug should call Cikl::Worker.logger.debug" do
-    expect(logger).to receive(:debug).with("this is an debug")
-    @obj.debug("this is an debug")
+  describe "when the logger is set to nil" do
+    before :each do
+      @obj = TestLogging.new
+      Cikl::Worker.logger = nil
+    end
+
+    specify "#error should not raise an error" do
+      expect { @obj.error("this is a an error") }.not_to raise_error
+    end
+    specify "#warn should not raise an warn" do
+      expect { @obj.warn("this is a an warn") }.not_to raise_error
+    end
+    specify "#info should not raise an info" do
+      expect { @obj.info("this is a an info") }.not_to raise_error
+    end
+    specify "#debug should not raise an debug" do
+      expect { @obj.debug("this is a an debug") }.not_to raise_error
+    end
   end
 
 
